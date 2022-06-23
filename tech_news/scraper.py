3478e79925bb9e43c35b291c27677665e5d165d2
Kristiano <kristianokasper@gmail.com>
2022-06-23 17:20:11 -0300

    Req 04

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -31,7 +31,17 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return {
+        "url": selector.css("link[rel=canonical]::attr(href)").get(),
+        "title": selector.css(".entry-title::text").get(),
+        "timestamp": selector.css(".meta-date::text").get(),
+        "writer": selector.css(".author a::text").get(),
+        "comments_count": 0,
+        "summary": selector.xpath("string(//p)").get(),
+        "tags": selector.css(".post-tags a::text").getall(),
+        "category": selector.css(".meta-category span.label::text").get(),
+    }
 
 
 # Requisito 5

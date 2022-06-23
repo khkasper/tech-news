e3270041a751eddb1dd37a9795569d800918601b
Kristiano <kristianokasper@gmail.com>
2022-06-23 16:05:05 -0300

    Req 03

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -24,7 +24,9 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    link = selector.css(".next.page-numbers::attr(href)").get()
+    return link
 
 
 # Requisito 4

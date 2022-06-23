b88241bd5decd3514d28f357006eb4591ce3a348
Kristiano <kristianokasper@gmail.com>
2022-06-23 01:11:40 -0300

    Req 01

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,17 @@
+import time
+import requests
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, headers={"user-agent": "Fake user-agent"})
+        time.sleep(1)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2

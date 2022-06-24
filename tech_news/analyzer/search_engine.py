18249cf28f8563a4ca18767ca9072ef1c247e5be
Kristiano <kristianokasper@gmail.com>
2022-06-24 01:36:33 -0300

    Req 07

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,20 @@
 from tech_news.database import search_news
+from datetime import datetime
+
+MONTHS = {
+    "01": "janeiro",
+    "02": "fevereiro",
+    "03": "março",
+    "04": "abril",
+    "05": "maio",
+    "06": "junho",
+    "07": "julho",
+    "08": "agosto",
+    "09": "setembro",
+    "10": "outubro",
+    "11": "novembro",
+    "12": "dezembro",
+}
 
 
 # Requisito 6
@@ -7,12 +23,24 @@ def search_by_title(title):
     title_results = []
     for news in news_list:
         title_results.append((news["title"], news["url"]))
+    print(news_list)
     return title_results
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    else:
+        year, month, day = date.split("-")
+        data = f"{int(day)} de {MONTHS[month]} de {year}"
+        news_list = search_news({"timestamp": data})
+        date_results = []
+        for news in news_list:
+            date_results.append((news["title"], news["url"]))
+        return date_results
 
 
 # Requisito 8

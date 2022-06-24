9f411652bee2082a4eb99c37affa20f0bd9cf8a0
Kristiano <kristianokasper@gmail.com>
2022-06-24 16:33:02 -0300

    Req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,23 @@
+from tech_news.database import db
+from pymongo import ASCENDING, DESCENDING
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    top_5 = []
+    news_list = (
+        db.news.find()
+        .sort([("comments_count", DESCENDING), ("title", ASCENDING)])
+        .limit(5)
+    )
+    for news in news_list:
+        top_5.append((news["title"], news["url"]))
+    return top_5
 
 
 # Requisito 11
 def top_5_categories():
     """Seu código deve vir aqui"""
+
+
+# https://stackoverflow.com/questions/8109122/how-to-sort-mongodb-with-pymongo
